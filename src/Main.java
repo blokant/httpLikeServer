aaddeca73301a635adfed06d35ac152a24e99b80
Antony <blokant@gmail.com>
2014-10-27 10:38:23 +0300

    gitignored build and prof files

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by trenkinan on 25.10.14.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Server server = new Server();
+    }
+}
--- src/Server.java
+++ src/Server.java
@@ -1,4 +1,6 @@
 
+import com.sun.xml.internal.ws.client.ClientSchemaValidationTube;
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -7,10 +9,14 @@ import java.net.Socket;
  * Слушает указанный в первом параметре порт и выводит все приходящие на него
  * сообщения на консоль.
  */
-public class Server {
+public class Server   {
 
-    public static void main(String[] args) {
-      /* Если аргументы отсутствуют, порт принимае значение поумолчанию */
+    private static final int DEFAULT_PORT = 8080;
+    private ServerSocket serverSocket;
+    private Socket clientSocket;
+
+    public Server() {
+               /* Если аргументы отсутствуют, порт принимае значение поумолчанию */
         int port = DEFAULT_PORT;
         ServerSocket serverSocket = null;
         try {
@@ -19,54 +25,68 @@ public class Server {
             System.out.println("Порт занят: " + port);
             System.exit(-1);
         }
-        Socket clientSocket = null;
-        try {
-            clientSocket = serverSocket.accept();
-        } catch (IOException e) {
-            System.out.println("Ошибка при подключении к порту: " + port);
-            System.exit(-1);
-        }
+
+    }
+    public void start() {
+
+    }
+
+    public void run() {
+
+        while (true) {
+
+
+            Socket clientSocket = null;
+            try {
+                clientSocket = serverSocket.accept();
+            } catch (IOException e) {
+                System.out.println("Ошибка при подключении к порту: " + port);
+                System.exit(-1);
+            }
       /*
        * Теперь можно получить поток ввода, в который помещаются сообщения от
        * клиента
        */
-        InputStream in = null;
-        OutputStream out = null;
-        try {
-            in = clientSocket.getInputStream();
-            out = clientSocket.getOutputStream();
-        } catch (IOException e) {
-            System.out.println("Не удалось получить поток ввода.");
-            System.exit(-1);
-        }
+            InputStream in = null;
+            OutputStream out = null;
+            try {
+                in = clientSocket.getInputStream();
+                out = clientSocket.getOutputStream();
+            } catch (IOException e) {
+                System.out.println("Не удалось получить поток ввода.");
+                System.exit(-1);
+            }
       /*
        * В этой реализации сервер будет без конца читать поток и выводить его
        * содержимое на консоль
        */
-        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-        String ln = null;
-        try {
-            while ((ln = reader.readLine()) != null && ln.length() != 0) {
-                System.out.println(ln + ": " + ln.length());
-                System.out.flush();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            String ln = null;
+            try {
+                while ((ln = reader.readLine()) != null && ln.length() != 0) {
+                    System.out.println(ln + ": " + ln.length());
+                    System.out.flush();
 
-                //break;
+                    //break;
+                }
+            } catch (IOException e) {
+                System.out.println("Ошибка при чтении сообщения.");
+                System.exit(-1);
             }
-        } catch (IOException e) {
-            System.out.println("Ошибка при чтении сообщения.");
-            System.exit(-1);
-        }
-        System.out.println("After getting the mesage");
-        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out));
-        try {
-            writer.write("HTTP/1.1 200 OK\n" + "\n"  +
-                    "<html>Hello world</html>\n");
-            writer.flush();
-        } catch (IOException ioe) {
+            System.out.println("After getting the mesage");
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out));
+            try {
+                writer.write("HTTP/1.1 200 OK\n" + "\n" +
+                        "<html>Hello world</html>\n");
+                writer.flush();
+            } catch (IOException ioe) {
 
+            }
         }
-    }
 
-    private static final int DEFAULT_PORT = 8080;
+
+      }
+
 }
+
 
